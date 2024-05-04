e3e700ce82608434088697d55d679bbab4b7e02b
witczakxd <111696349+witczakxd@users.noreply.github.com>
2024-05-05 00:17:05 +0200

    Deleted unnecessary comments Fixed possible cv2.destroyAllWindows bug

--- Data_preprocessor/main.py
+++ Data_preprocessor/main.py
@@ -12,9 +12,6 @@ class HandDetector:
                                          min_tracking_confidence=0.5)
         self.mp_drawing = mp.solutions.drawing_utils
 
-    # funkcja ta dziala dla jednej klatki
-    # oznaczanie punktow, nadanie im nazw a1,a2, itp
-    # zapisywanie do hand_data punktow w postaci klucz:wartosc - jest to ich polozenie wzgledem punktu 0,0 w lewym gornym rogu
     def label_points(self, image, landmarks):
         hand_data = {}
         for idx, landmark in enumerate(landmarks.landmark):
@@ -25,8 +22,6 @@ class HandDetector:
             hand_data[label] = (landmark.x, landmark.y, landmark.z)
         return image, hand_data
 
-    # oznaczanie dloni na klatce, obecnie w klatce moze byc wiecej niz jedna dlon, chyba powinnismy to ograniczyc tylko do jednej dloni
-    # ta funkcja znajduje punkty na klatce i przekazuje je do funkcji ktora je oznacza i zwraca
     def detect_hands(self, frame):
         results = self.hands.process(frame)
         hand_positions = []
@@ -47,14 +42,12 @@ class VideoProcessor:
         self.hand_detector = HandDetector()
         self.display = True
 
-    # przetwarza filmik
     def process_video(self, video_path):
         cap = cv2.VideoCapture(video_path)
         if not cap.isOpened():
             print("Error: Cannot open video file:", video_path)
             return
 
-        # pozycje dloni na wszystkich klatkach
         hand_positions = []
         label = str(os.path.abspath(video_path)).split("\\")[-2]
 
@@ -80,24 +73,10 @@ class VideoProcessor:
         os.makedirs(output_dir, exist_ok=True)
         output_path = os.path.join(output_dir, 'output.csv')
 
-        # Saving the processed data to a CSV file
         self.save_to_csv(hand_positions, output_path)
         print("Processed and data saved:", output_path)
 
-        # w tym momencie w hand_positions znajduja sie wszystkie pozycje dloni w danym filmie
-        # trzeba je teraz przetworzyc i zapisac do pliku
-        # trzeba sprawdzic czy w danym elemencie hand_positions znajduje sie 21 elementow tzn, czy zaden punkt dloni nie zostal uciety np byl poza kamera
-
-        # w tym momencie mamy punkty z jednego video z jednego folderu, chyba najlepiej najpierw przetworzyc wszystkie pliki osobno, tzn zapisywac np filmik1 z folderu A
-        # do folderu Data_przetworzone/A_przetworzone/filmik1_przetworzone.csv
-        # a potem polaczyc te wszystkie pliki csv w calosc
-        # w csv powinno byc cos w tym stylu
-        # label,a1a5(odleglosc od a1 do a5),...
-        # A,0.5784,...
-        # cos w tym formacie
-
         print("Processed: ", video_path)
-        cv2.destroyAllWindows()
         cap.release()
 
     def save_to_csv(self, hand_positions, output_path):
@@ -105,13 +84,11 @@ class VideoProcessor:
             print("No hand positions data to save.")
             return
 
-        # Creating DataFrame to save as CSV
         df = pd.DataFrame([pos for frame_data in hand_positions for pos in frame_data])
         df.to_csv(output_path, index=False)
 
         print("Data saved to:", output_path)
 
-    # przetwarza wszystkie filmiki w danym folderze
     def process_videos_in_folder(self, folder_path):
         if not os.path.exists(folder_path):
             print(f"The folder {folder_path} does not exist.")
@@ -127,7 +104,6 @@ class VideoProcessor:
         self.hand_detector.close()
 
 
-# przetwarza wszystkie foldery w folderze Data
 def process_folders_in_data(data_folder_path, multithreading=False):
     subfolders = [folder for folder in os.listdir(data_folder_path) if
                   os.path.isdir(os.path.join(data_folder_path, folder))]
@@ -142,7 +118,6 @@ def process_folders_in_data(data_folder_path, multithreading=False):
             process_folder(folder_path)
 
 
-# przetwarzanie folderu
 def process_folder(folder_path):
     video_processor = VideoProcessor()
     video_processor.process_videos_in_folder(folder_path)
@@ -152,16 +127,14 @@ def combine_csv():
     data_preprocessor_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "..", "Data_preprocessor")
     processed_data_path = os.path.join(data_preprocessor_path, "Processed_Data")
 
-    combined_dataframe = pd.DataFrame()  # Initialize an empty DataFrame to hold all the data
+    combined_dataframe = pd.DataFrame()
     if os.path.exists(processed_data_path) and os.path.isdir(processed_data_path):
-        # List all subdirectories in the 'Processed_Data'
         subdirs = [os.path.join(processed_data_path, d) for d in os.listdir(processed_data_path) if
                    os.path.isdir(os.path.join(processed_data_path, d))]
         for subdir in subdirs:
             csv_file_path = next((os.path.join(subdir, file) for file in os.listdir(subdir) if file.endswith('.csv')),
                                  None)
             if csv_file_path:
-                # Read the CSV file and append it to the DataFrame
                 temp_df = pd.read_csv(csv_file_path)
                 combined_dataframe = pd.concat([combined_dataframe, temp_df], ignore_index=True)
     else:
